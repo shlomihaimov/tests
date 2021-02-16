5ba5c230dd3ce4d4dfb10aa3e5dab122f981c7e6
Ubuntu <ubuntu@ip-172-31-19-107.eu-central-1.compute.internal>
2021-02-16 09:45:28 +0000

    fixed

---
+++ add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add(a,b):
+    return a+b
+
+#def main():
+#    a = int(input("Enter a: "))
+#    b = int(input("Enter b: "))
+    
+ #   print("the sum is ",add(a,b))
+
+#if __name__ == "__main__":
+ #   main()
+
+
---
+++ test_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import add
+
+def test_add():
+    assert add.add(2,2)==4
+
+def main():
+    test_add()
+
+if __name__ == "__main__":
+        main()
+
+
